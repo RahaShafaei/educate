aea566007de20961b961ea39b7fcdfc097cc27b4
Raha <fatemeshafaei@gmail.com>
2023-10-25 14:05:38 +0330

    base created

---
+++ src/main/java/edu/educate/EducateApplication.java
@@ -0,0 +1,14 @@
+package edu.educate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducateApplication.class, args);
+        System.out.println("Hello Edu");
+    }
+
+}
