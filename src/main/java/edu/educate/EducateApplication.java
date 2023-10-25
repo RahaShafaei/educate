92b4d52e3e510e23ea3332ab90caacb2cb9f99b1
Raha <fatemeshafaei@gmail.com>
2023-10-25 15:30:42 +0330

    sec11

--- src/main/java/edu/educate/EducateApplication.java
+++ src/main/java/edu/educate/EducateApplication.java
@@ -8,7 +8,7 @@ public class EducateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EducateApplication.class, args);
-        System.out.println("Hello");
+        System.out.println("Hello1");
     }
 
 }
