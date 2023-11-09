879f5ee31887d288910693838b8db83cc64255e9
Raha <fatemeshafaei@gmail.com>
2023-11-09 18:10:33 +0330

    Provide template layout

--- src/main/java/edu/educate/configurations/EducateConfigs.java
+++ src/main/java/edu/educate/configurations/EducateConfigs.java
@@ -1,10 +1,13 @@
 package edu.educate.configurations;
 
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Configuration
 @PropertySource("classpath:ValidationMessages.properties")
@@ -18,5 +21,9 @@ public class EducateConfigs {
         return messageSource;
     }
 
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 
 }
